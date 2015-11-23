e206585cef81450eae03606e83fef311c7e1d1ef
benKarpol <benkarpol@gmail.com>
2015-11-23 11:43:13 +0200

    added getBooksByCategory

--- src/model/backend/BackendImp.java
+++ src/model/backend/BackendImp.java
@@ -60,6 +60,14 @@ public class BackendImp implements Backend{
     }
 
     @Override
+    public Book SearchBook(int bookId) throws Exception{
+        for (Book B : getBooks())
+            if (B.getBookId() == bookId)
+                return B;
+        throw (new Exception("The book number " + bookId + " doesn't exist."));
+    }
+
+    @Override
     public void addSupplier(Supplier aSupplier) throws Exception {
         aSupplier.setSuppID(++lastSupplierBookId);
         data.suppliers.add(aSupplier);
@@ -157,5 +165,24 @@ public class BackendImp implements Backend{
     @Override
     public void deleteSupplierBook(int supplierBookId) {
         data.supplierBooks.get(supplierBookId).setDeleted(true);
+    }
+
+    /**
+     * search book by category in the SupplierBooks list
+     * @param category
+     * @return BooksByCategory, new list of all books in this category
+     * @throws Exception if the new list is empty, no found books in this category
+     */
+    public ArrayList<SupplierBook> getBooksByCategory(BookCategory category) throws Exception
+    {
+        ArrayList<SupplierBook> BooksByCategory = new ArrayList<SupplierBook>();
+        for (SupplierBook B : getSupplierBooks())
+        {
+            if (SearchBook(B.getBookId()).getCategory() == category)
+                BooksByCategory.add(B);
+        }
+        if (BooksByCategory == null) 
+            throw (new Exception("no books in this category"));
+        return BooksByCategory;
     }
 }
